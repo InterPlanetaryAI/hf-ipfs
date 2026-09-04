189e3c32b05f7f111384db8e458a7b7b32f1b194
Roberto C. Morano <roberto.morano@veridian.id>
2026-09-04 01:13:27 +0000

    chore(ci): release-please pipeline with cross-built release assets

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ import (
 	"github.com/ipai/hf-ipfs/internal/wire"
 )
 
-const version = "0.1.0"
+var version = "0.1.0" // overridden at release time via -ldflags -X main.version
 
 func main() {
 	if err := run(os.Args[1:]); err != nil {
